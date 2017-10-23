b3c64a723b6f708f772dfbb031d13e5849fff104
Flavien DELANGLE <fdelangle@elv.telecom-lille.fr>
2017-10-23 20:48:46 +0200

    Render Movie Overlay (Grid layout) only on hover

--- frontend/src/scenes/CollectionMovies/components/MoviesData/components/Grid/components/Movie/index.js
+++ frontend/src/scenes/CollectionMovies/components/MoviesData/components/Grid/components/Movie/index.js
@@ -10,27 +10,48 @@ import './style.css'
 
 class Movie extends Component {
   
+  state = {
+    mouseOver: false
+  };
+  
+  handleMouseHover = mouseOver => {
+    this.setState({mouseOver})
+  };
+  
+  renderOverlay = () => {
+    if(this.state.mouseOver) {
+      return (
+        <div className="overlay">
+          <Link to={'/movies/' + this.props.data.tmdbId + '/'}>
+            <div className="overlay-main">
+              <div className="title">{this.props.data.title}</div>
+              <div className="note">
+                <StarRatingComponent
+                  name={"Rate " + this.props.data.title}
+                  starCount={10}
+                  value={this.props.data.note}
+                  editing={false}
+                />
+              </div>
+            </div>
+          </Link>
+          <Actions data={this.props.data} type='collection_movies'/>
+        </div>
+      )
+    } return (null);
+  };
+  
   render() {
     return (
       <div className="movie-container">
-        <Paper zDepth={3} className="movie">
+        <Paper
+          zDepth={3}
+          className="movie"
+          onMouseEnter={() => this.handleMouseHover(true)}
+          onMouseLeave={() => this.handleMouseHover(false)}
+        >
           <Poster path={this.props.data.poster} title={this.props.data.title} />
-          <div className="overlay">
-            <Link to={'/movies/' + this.props.data.tmdbId + '/'}>
-              <div className="overlay-main">
-                <div className="title">{this.props.data.title}</div>
-                <div className="note">
-                  <StarRatingComponent
-                    name={"Rate " + this.props.data.title}
-                    starCount={10}
-                    value={this.props.data.note}
-                    editing={false}
-                  />
-                </div>
-              </div>
-            </Link>
-            <Actions data={this.props.data} type='collection_movies'/>
-          </div>
+          {this.renderOverlay()}
         </Paper>
       </div>
     );
