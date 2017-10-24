8ac12a7d865ddfbf06365bee94a7e1f8376ed0b7
Flavien DELANGLE <fdelangle@elv.telecom-lille.fr>
2017-10-24 18:17:51 +0200

    Change Movie horizontal scroll for a slick

--- frontend/src/scenes/Movie/components/Recommendations/index.js
+++ frontend/src/scenes/Movie/components/Recommendations/index.js
@@ -1,20 +1,78 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import ScrollArea from 'react-scrollbar'
+import Slider from 'react-slick'
+
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 
 import { path } from '../../../../services/TheMovieDatabaseJS/images'
 
+import './style.css'
+
 const containerStyle = {
-  height: 128,
-  overflowY: 'hidden',
-  whiteSpace: 'nowrap',
-  width: '90%',
-  margin: '30px auto 0 auto'
+  margin: '0 auto',
+  padding: 40,
+  width: '80%',
+  maxWidth: 1200,
+  color: '#333',
+};
+
+const titleStyle = {
+  fontSize: '2em',
+  textAlign: 'left',
+  margin: '25px 0px'
 };
 
 const movieStyle = {
-  display: 'inline-block',
-  padding: 10,
+};
+
+const settings = {
+  dots: true,
+  infinite: false,
+  speed: 500,
+  slidesToShow: 6,
+  slidesToScroll: 6,
+  initialSlide: 0,
+  responsive: [
+    {
+      breakpoint: 1450,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        infinite: true,
+        dots: true
+      }
+    },{
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        infinite: true,
+        dots: true
+      }
+    },{
+      breakpoint: 975,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },{
+      breakpoint: 710,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2
+      }
+    },{
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
 };
 
 class Recommendations extends Component {
@@ -39,14 +97,10 @@ class Recommendations extends Component {
   render() {
     return (
       <div style={containerStyle}>
-        <ScrollArea
-          speed={0.8}
-          vertical={false}
-        >
-          <div>
-            {this.renderRecommendations()}
-          </div>
-        </ScrollArea>
+        <div style={titleStyle}>Recommendations :</div>
+        <Slider {...settings}>
+          {this.renderRecommendations()}
+        </Slider>
       </div>
     );
     
